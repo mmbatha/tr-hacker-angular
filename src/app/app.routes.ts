7c2b3a7b9a2272a938258f57f756d67a1e735fb4
Monde M. <mmbatha@tracker.co.za>
2025-05-14 07:44:02 +0200

    Creates folders for components and store. Added components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StoryComponent } from './components/story/story.component';
+import { UserComponent } from './components/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'item/:id', component: StoryComponent },
+  { path: 'user/:id', component: UserComponent },
+  { path: '**', redirectTo: '' }
+];
