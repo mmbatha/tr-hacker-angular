171936a8306b03418c4bf8abb0153980e24eb698
Monde M. <mmbatha@tracker.co.za>
2025-05-14 13:01:32 +0200

    Adds the effects, actions and reducers.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,9 +11,11 @@ import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { storyReducer } from './store/reducers/story.reducer';
+import { StoryEffects } from './store/effects/story.effects';
 
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(), provideHttpClient(), provideNzI18n(en_GB), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(), provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(storyReducer), provideHttpClient(), provideNzI18n(en_GB), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideEffects([StoryEffects]), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
