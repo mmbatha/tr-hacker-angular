fe965c648d460c0f5a82f51defa4f53ef036e7b2
Monde M. <mmbatha@tracker.co.za>
2025-05-15 09:29:07 +0200

    Fixes issue noted here: https://github.com/ngrx/platform/issues/3654

--- src/app/store/effects/story.effects.ts
+++ src/app/store/effects/story.effects.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { HttpClient } from '@angular/common/http';
 import { map, catchError, concatMap } from 'rxjs/operators';
@@ -8,15 +8,14 @@ import { StoryActions } from '../actions/story.actions';
 
 @Injectable()
 export class StoryEffects {
-  constructor(private actions$: Actions, private http: HttpClient) {
-    console.log('HTTP client injected:', this.http);
-  }
+  private actions$ = inject(Actions);
+  constructor(private http: HttpClient) { }
 
   loadTopStories$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(StoryActions.loadTopStories),
       concatMap(() =>
-        this.http.get<number[]>('http://hacker-news.firebaseio.com/v0/topstories.json').pipe(
+        this.http.get<number[]>('https://hacker-news.firebaseio.com/v0/topstories.json').pipe(
           map(ids => StoryActions.loadTopStoriesSuccess({ ids: ids.slice(0, 20) })),
           catchError(error => of(StoryActions.loadTopStoriesFailure({ error })))
         )
