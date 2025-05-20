b1c5e5444211bbf198cf1436da1c390a830a468b
Monde M. <mmbatha@tracker.co.za>
2025-05-20 11:01:14 +0200

    Changes in progress.

--- src/app/services/story.service.spec.ts
+++ src/app/services/story.service.spec.ts
@@ -1,20 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StoryService } from './story.service';
-import { of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 
 describe('StoryService', () => {
   let service: StoryService;
 
-  const mockService = {
-    get: () => of({})
-  };
-
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [{ provide: HttpClient, useValue: mockService }]
-    });
+    TestBed.configureTestingModule({});
     service = TestBed.inject(StoryService);
   });
 
